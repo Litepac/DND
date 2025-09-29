cf0093e3722124598969aa879784075df546a06f
Litepac <90896756+Litepac@users.noreply.github.com>
2025-09-29 12:29:27 +0200

    Opdateret program ret meget - tjek ændringer

--- DNDProject.Api/Program.cs
+++ DNDProject.Api/Program.cs
@@ -1,20 +1,67 @@
 using DNDProject.Api.Data;
+using DNDProject.Api.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// EF Core + SQLite
 builder.Services.AddDbContext<AppDbContext>(o =>
     o.UseSqlite(builder.Configuration.GetConnectionString("Default")));
 
+// Controllers
 builder.Services.AddControllers();
 
+// CORS (åben i dev – stram til senere)
 builder.Services.AddCors(opt =>
 {
-    opt.AddDefaultPolicy(p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+    opt.AddDefaultPolicy(p => p
+        .AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod());
 });
 
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+// Identity (brugere/roller) via EF
+builder.Services
+    .AddIdentityCore<ApplicationUser>(opt =>
+    {
+        opt.Password.RequiredLength = 6;
+        opt.Password.RequireDigit = false;
+        opt.Password.RequireUppercase = false;
+        opt.Password.RequireNonAlphanumeric = false;
+    })
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddSignInManager();
+
+// JWT config (nøgler hentes fra appsettings.json -> "Jwt")
+var jwt = builder.Configuration.GetSection("Jwt");
+var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt["Key"]!));
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opt =>
+    {
+        opt.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwt["Issuer"],
+            ValidAudience = jwt["Audience"],
+            IssuerSigningKey = key
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -24,9 +71,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-// Brug http lige nu, så hold evt. HTTPS-redirect slukket, eller kør også HTTPS på API'et
-// app.UseHttpsRedirection();
-
 app.UseCors();
+
+// VIGTIGT: auth før MapControllers
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 app.Run();
